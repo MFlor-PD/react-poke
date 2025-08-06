d9dadc65648c1b9aa14a6ea602d6e86b2eab3419
Flor <mariaflorenciaperezd@gmail.com>
2025-08-06 10:36:05 +0200

    cambie de base: '/react-poke/' a base: './'

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/react-poke/',  // <-- esta línea es clave para GitHub Pages
+  base: '/',  // <-- esta línea es clave para GitHub Pages
   plugins: [react()],
 })
 
